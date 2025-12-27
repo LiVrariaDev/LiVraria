52102cd6821203cd3e5d8f05fd48ed3204973801
Kaeru-Amber <minecraftkaerunomoto@outlook.jp>
2025-12-27 22:38:31 +0900

    fix: pyscardのversionが古い

--- raspi/nfc/setup.sh
+++ raspi/nfc/setup.sh
@@ -63,7 +63,8 @@ else
 fi
 
 source .venv/bin/activate
-pip install --upgrade pip
+pip install --upgrade pip setuptools wheel
+pip install git+https://github.com/LudovicRousseau/pyscard.git@master
 pip install -r requirements.txt
 echo "依存関係のインストールが完了しました"
 
